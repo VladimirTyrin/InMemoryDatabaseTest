27efc75126028b9f3016ab8b850a1a3a334d68c7
Vladimir.Tyrin <b0-0b@yandex.ru>
2016-12-16 18:14:13 +0300

    Example added

---
+++ InMemoryDatabaseTest/InMemoryContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InMemoryDatabaseTest.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InMemoryDatabaseTest
+{
+    public class InMemoryContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase();
+            }
+        }
+
+        public DbSet<BaseEntity> BaseEntities { get; set; }
+    }
+}
---
+++ InMemoryDatabaseTest/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using InMemoryDatabaseTest.Entities;
+
+namespace InMemoryDatabaseTest
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public ObservableCollection<BaseEntity> Entities = new ObservableCollection<BaseEntity>();
+
+        public void Update()
+        {
+            using (var context = new InMemoryContext())
+            {
+                var allNew = context.BaseEntities.ToList();
+                Entities.Clear();
+                foreach (var baseEntity in allNew)
+                {
+                    Entities.Add(baseEntity);
+                }
+            }
+        }
+
+        public void AddWithName(string name)
+        {
+            using (var context = new InMemoryContext())
+            {
+                context.BaseEntities.Add(new BaseEntity
+                {
+                    Name = name
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
